8fccfa443dbc1fe027c32663fd945073cca40362
Lenize Ramos <lenizemr@gmail.com>
2024-10-20 19:07:18 -0700

    ex01 completed

--- dist/exercises.js
+++ dist/exercises.js
@@ -9,6 +9,22 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+    average() {
+        if (!this.head) {
+            return 0;
+        }
+        let sum = 0;
+        let count = 0;
+        let current = this.head;
+        while (current) {
+            if (typeof current.value === "number") {
+                sum += current.value;
+                count++;
+            }
+            current = current.next;
+        }
+        return count > 0 ? sum / count : 0;
+    }
     insertAtBack(value) {
         const newNode = new ListNode(value);
         if (!this.head) {
@@ -26,4 +42,4 @@ const list = new LinkedList();
 list.insertAtBack(1);
 list.insertAtBack(2);
 list.insertAtBack(3);
-console.log(list);
+console.log(list.average());
--- exercises/exercises.ts
+++ exercises/exercises.ts
@@ -14,6 +14,26 @@ class LinkedList<T> {
     this.head = null;
   }
 
+  average(): number {
+    if (!this.head) {
+      return 0;
+    }
+
+    let sum = 0;
+    let count = 0;
+    let current: ListNode<T> | null = this.head;
+
+    while (current) {
+      if (typeof current.value === "number") {
+        sum += current.value;
+        count++;
+      }
+      current = current.next;
+    }
+
+    return count > 0 ? sum / count : 0;
+  }
+
   insertAtBack(value: T): void {
     const newNode = new ListNode(value);
     if (!this.head) {
@@ -33,3 +53,4 @@ const list = new LinkedList<number>();
 list.insertAtBack(1);
 list.insertAtBack(2);
 list.insertAtBack(3);
+console.log(list.average());
